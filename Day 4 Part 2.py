260b2c1ce4aede8dd3606c28f18051cb049868e0
Ariadne Thompson <43646932+AriadneThompson@users.noreply.github.com>
2019-12-04 11:24:41 +0000

    Added day four.

---
+++ Day 4 Part 2.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+
+def is_valid(password):
+    double_digit = False
+    
+    for i in range(5):
+        if password[i] == password[i+1] and password.count(password[i]) == 2:
+            double_digit = True
+        if password[i] > password[i+1]:
+            return False
+        
+    return double_digit
+    
+    
+
+total_valid = 0
+
+for i in range(248345, 746315):
+    if is_valid(str(i)):
+        total_valid += 1
+        
+print(total_valid)
